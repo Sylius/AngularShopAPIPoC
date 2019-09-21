98b1aada38a53db8e9a5fb67ae67dd4b1734485e
Robert Kitzing <robert.kitzing@brille24.de>
2019-09-21 11:57:08 +0200

    added bootstrap

--- sylius-shop-api-angular-ui/src/app/app.module.ts
+++ sylius-shop-api-angular-ui/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { RegisterComponent } from './register/register.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
